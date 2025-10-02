ae0d355fab6c0a60d1542723a830c40f5f875f9d
Oleg <zhdanov.oleg.2000@gmail.com>
2025-10-02 10:50:56 +0300

    Initial project

---
+++ src/main/java/ru/toakongu/todo/MainApplication.java
@@ -0,0 +1,9 @@
+package ru.toakongu.todo;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+//        HttpServer server = new HttpServer(8080);
+//        server.start();
+    }
+}
